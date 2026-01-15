dea4d9da8aacb54c434ec1e8c2f0380d47601a01
ORyXCc <67155862+ORyXCc@users.noreply.github.com>
2026-01-15 10:27:29 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/academic-project-page-template-vue',
+  base:'/CE-SARTEST',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
